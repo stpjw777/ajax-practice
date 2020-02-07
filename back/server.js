e9e5a9272ad8d2a259f06de90df4be5971868c2a
jaewan <stpjw777@gmail.com>
2020-02-07 10:20:18 +0900

    xhr get

--- back/server.js
+++ back/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const port = 8001;
+
+app.use(cors());
+
 // app.get("/", (req, res) => {
 //   return res.send("Hello Node.js");
 // });
 app.get("/", (req, res) => res.send("Hello Node.js"));
+app.get("/time", (req, res) => {
+  time = Date();
+  res.send(time);
+  console.log("connected");
+});
+
 app.listen(port, () => console.log(`server running ... Port : ${port}`));
