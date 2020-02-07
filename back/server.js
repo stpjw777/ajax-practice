e4bbeb6ba0a50cd908d19a725b91c60f5466cb26
jaewan <stpjw777@gmail.com>
2020-02-07 09:56:46 +0900

    server setting

---
+++ back/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+const port = 8001;
+// app.get("/", (req, res) => {
+//   return res.send("Hello Node.js");
+// });
+app.get("/", (req, res) => res.send("Hello Node.js"));
+app.listen(port, () => console.log(`server running ... Port : ${port}`));
